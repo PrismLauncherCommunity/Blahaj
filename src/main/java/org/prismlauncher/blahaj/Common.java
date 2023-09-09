f1bb8e1f46b595c7511a463b57b18bcde0b02e5f
IThundxr <contact@ithundxr.dev>
2023-09-09 10:28:22 -0400

    basically done

--- src/main/java/org/prismlauncher/blahaj/Common.java
+++ src/main/java/org/prismlauncher/blahaj/Common.java
@@ -5,8 +5,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.minecraft.core.Registry;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.core.registries.Registries;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
@@ -18,7 +17,6 @@ import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
-import org.intellij.lang.annotations.Identifier;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -28,16 +26,16 @@ public class Common implements ModInitializer {
 
 	public void onInitialize() {
 		Item grayShark = new CuddlyItem(new Item.Properties().stacksTo(1), "item.blahaj.gray_shark.tooltip");
-		Registry.register(RegistryKeys.ITEM, asResource("gray_shark"), grayShark);
+		Registry.register(BuiltInRegistries.ITEM, asResource("gray_shark"), grayShark);
 
 		Item blueShark = new CuddlyItem(new Item.Properties().stacksTo(1), "item.blahaj.blue_shark.tooltip");
-		Registry.register(RegistryKeys.ITEM, asResource("blue_shark"), blueShark);
+		Registry.register(BuiltInRegistries.ITEM, asResource("blue_shark"), blueShark);
 
 		Item blueWhale = new ItemContainerCuddlyItem(new Item.Properties().stacksTo(1), "item.blahaj.blue_whale.tooltip");
-		Registry.register(RegistryKeys.ITEM, asResource("blue_whale"), blueWhale);
+		Registry.register(BuiltInRegistries.ITEM, asResource("blue_whale"), blueWhale);
 
 		Item breadPillow = new CuddlyItem(new Item.Properties().stacksTo(1), null);
-		Registry.register(RegistryKeys.ITEM, asResource("bread"), breadPillow);
+		Registry.register(BuiltInRegistries.ITEM, asResource("bread"), breadPillow);
 
 		// Register items to item group
 		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register((content) -> {
